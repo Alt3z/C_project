57085493af5c7c547bd7ef51c18c399e2a61c084
Фёдор Онищенко <166149674+Alt3z@users.noreply.github.com>
2024-04-06 02:29:41 +0500

    Add files via upload

---
+++ Работа с потоками/2 часть/Lab 2_3.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+float summa = 0;
+const int n = 10;
+const int m = 10;
+const float a = 0, b = 10;
+
+CRITICAL_SECTION cs;
+
+struct Node 
+{
+    int number_n;
+    int number_m;
+    float size_block;
+    float size_thread;
+};
+
+DWORD WINAPI Sum(LPVOID param)
+{
+    Node* pcol_num = (Node*)param;
+    Node mas = *pcol_num;
+
+    float StartX = (mas.number_n * mas.size_block) + (mas.number_m * mas.size_thread) + a;
+    float EndX = (mas.number_n * mas.size_block) + (mas.number_m * mas.size_thread) + a + mas.size_thread;
+
+    int height = 4;
+    float StartY = StartX + height;
+    float EndY = EndX + height;
+
+    float max = EndY;
+    if (StartY > EndY)
+    {
+        max = StartY;
+    }
+
+    EnterCriticalSection(&cs);
+    summa += max * mas.size_thread;
+    LeaveCriticalSection(&cs);
+
+    return 0;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    
+    HANDLE hThread[m];
+    DWORD dwThreadID[m];
+
+    InitializeCriticalSection(&cs);
+
+    float size = b - a; //длина отрезка
+    float size_block = size / (float)n; // длина кусков отрезока
+    float size_thread = size_block / (float)m;
+
+    Node* mas { new Node[m * n] };
+
+    for (int i = 0; i < m; i++) 
+    {
+        mas[i].number_m = i;
+        mas[i].size_block = size_block;
+        mas[i].size_thread = size_thread;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++) 
+        {
+            mas[j].number_n = i;
+            hThread[j] = CreateThread(NULL, 0, Sum, &(mas[j]), 0, &(dwThreadID[j]));
+
+            if (hThread[j] == NULL)
+            {
+                cout << "Поток № " << i << "не был создан\n" << "Ошибка " << GetLastError();
+            }
+        }
+
+        WaitForMultipleObjects(m, hThread, true, INFINITE);
+    }
+
+    for (int i = 0; i < m; i++) // закрываем потоки
+    {
+        CloseHandle(hThread[i]);
+    }
+
+    cout <<"Площадь: " << summa << endl;
+}
---
+++ Работа с потоками/2 часть/Lab_2_2.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int n = 0;
+int k = 5;
+string text = "asdhfgjk jsdhf kskskjdwerf";
+//float summa = 0;
+float* sum = new float[k];
+
+CRITICAL_SECTION cs;
+
+DWORD WINAPI FillingMatrixRows(LPVOID param) // функция для заполнения строк матрицы
+{
+    int* pNumberOfCol = (int*)param;
+    int NumberOfThread = *pNumberOfCol;
+
+    int s = 0;
+    int NubmerOfChar = NumberOfThread + k * s;
+    while (NubmerOfChar<text.length())
+    {
+        EnterCriticalSection(&cs);
+        //summa += int(text[NubmerOfChar]);
+        sum[NumberOfThread] += int(text[NubmerOfChar]);
+        LeaveCriticalSection(&cs);
+
+        s++;
+        NubmerOfChar = NumberOfThread + k * s;
+    }
+
+    return 0;
+}
+
+
+void main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    n = text.length();
+    while (k >= n)
+    {
+        k--;
+    }
+
+    int* number = new int[k];
+    for (int i = 0; i < k; i++)
+    {
+        number[i] = i;
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        sum[i] = 0;
+    }
+
+    HANDLE* FillingThread = new HANDLE[k];
+    DWORD* FillingThreadID = new DWORD[k];
+
+    InitializeCriticalSection(&cs);
+
+
+    for (int i = 0; i < k; i++) // запуск потоков для заполнения строк
+    {
+        FillingThread[i] = CreateThread(NULL, 0, FillingMatrixRows, &(number[i]), 0, &(FillingThreadID[i]));
+        //FillingThread[i] = CreateThread(атрибуты безопасности по умолчанию, размер стека по умолчанию,имя функции, указатель на параметры, флаг создания = 0, адрес переменной для идентификатора)
+
+        if (FillingThread[i] == NULL) // Проверям - создан ли поток
+        {
+            cout << "Поток №" << i << "не был создан\n" << "Ошибка: " << GetLastError();
+        }
+    }
+
+    WaitForMultipleObjects(k, FillingThread, true, INFINITE); // Ожидаем завершения потоков
+
+    for (int i = 0; i < k; i++) // закрываем потоки
+    {
+        CloseHandle(FillingThread[i]);
+    }
+
+    //cout << endl << "Сумма элементов: " << summa/256 << endl;
+
+    float g=0;
+    for (int i = 0; i < k; i++)
+    {
+        if (sum[i] != INT_MAX)
+        {
+            g += sum[i];
+        }
+    }
+    cout << "Сумма элементов по модулю 256: " << g/256 << endl;
+    DeleteCriticalSection(&cs);
+}
